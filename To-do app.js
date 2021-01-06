e08f076105456384fd2f8b8326947edf60302203
Matthew Mcdonald <43245896+Matthew-Mcds@users.noreply.github.com>
2021-01-06 15:27:58 -0500

    Add files via upload

--- To-do app.js
+++ To-do app.js
@@ -1,3 +1,21 @@
 const dateElement = document.getElementById(date);
 const listElement = document.getElementById(itemList);
 const inputElement = document.getElementById(input);
+
+
+
+function newTodo(toDo){
+
+    const position = "beforeend";
+
+
+
+    const itemText = `<li class="singleItem">
+<i class="glyphicon glyphicon-cloud"></i>
+<p class="text">Look at cloud</p>
+<i class="glyphicon glyphicon-remove"></i>
+</li>`
+
+ listElement.insertAdjacentHTML(position,itemText);
+
+}
